eaf020f40baca6318284ac63d71945c568b0ced3
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2024-02-07 00:15:07 +0530

    feat: update `TextOptions` and `InputFieldOptions`

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -3,11 +3,17 @@ export type ViewOptions = {
   className?: string;
 };
 
-export type TextOptions =  {
-  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
+export type TextOptions = {
+  type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
 } & ViewOptions;
+
 export type ButtonOptions = {} & ViewOptions;
-export type InputFieldOptions = {} & ViewOptions;
+
+export type InputFieldOptions = {
+  type?: "number" | "password" | "text";
+  maxLength?: number;
+} & ViewOptions;
+
 export type ContainerOptions = {} & ViewOptions;
 
 export type LeadGenOptions = {} & WidgetOptions;
